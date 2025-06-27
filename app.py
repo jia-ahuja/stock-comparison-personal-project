b70e5765f9d33766a356fca5423241f6dd8be736
Jiya Ahuja <jia.ahujacod@gmail.com>
2025-06-27 13:48:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,42 +7,43 @@ stock_name = st.text_input('Stock name')
 
 options = st.multiselect(
     "Columns to compare",
-    ['valuation', 'grade', 'quad', 'valuscor', 'cmp', 'pe', 'fv', 'eq', 'dy', 'np2eq', 'cmp2npeq', 'ttmsal', 'salfy', 'salq0', 'salq1', 'ttmnp', 'npfy', 'npq0', 'npq1', 'ttmeps', 'epsfy', 'epsq0', 'epsq1', 'roe3y', 'roa3y', 'roce3y', 'debt', 'intcov', 'db2eq', 'ncffy', 'fcffy', 'ev2fcf', 'ph']
+    ['valuation', 'grade', 'quad', 'valuscor', 'cmp', 'pe', 'fv', 'eq', 'dy', 'np2eq', 'cmp2npeq', 'ttmsal', 'salfy', 'salq0', 'salq1', 'ttmnp', 'npfy', 'npq0', 'npq1', 'ttmeps', 'epsfy', 'epsq0', 'epsq1', 'roe3y', 'roa3y', 'roce3y', 'debt', 'intcov', 'db2eq', 'ncffy', 'fcffy', 'ev2fcf', 'ph'],
+    default=['valuation', 'grade', 'quad', 'valuscor', 'cmp']
 )
 
 
 if st.button('Get data'):
     if stock_name and options:
         try:
-            df_2022 = pd.read_excel('file1.xlsx')  
-            df_2023 = pd.read_excel('file2.xlsx') 
-            df_2024 = pd.read_excel('file3.xlsx') 
+            df_jan = pd.read_excel('file1.xlsx')  
+            df_apr = pd.read_excel('file2.xlsx') 
+            df_jun = pd.read_excel('file3.xlsx') 
 
 
-            stock_2022 = df_2022[df_2022['name'].str.contains(stock_name, case=False, na=False)]
-            stock_2023 = df_2023[df_2023['name'].str.contains(stock_name, case=False, na=False)]
-            stock_2024 = df_2024[df_2024['name'].str.contains(stock_name, case=False, na=False)]
+            stock_jan = df_jan[df_jan['name'].str.contains(stock_name, case=False, na=False)]
+            stock_apr = df_apr[df_apr['name'].str.contains(stock_name, case=False, na=False)]
+            stock_jun = df_jun[df_jun['name'].str.contains(stock_name, case=False, na=False)]
             
             comparison_data = []
             
             for column in options:
                 row = {
                     'Metric': column,
-                    '2022': stock_2022[column].iloc[0] if len(stock_2022) > 0 and column in stock_2022.columns else 'N/A',
-                    '2023': stock_2023[column].iloc[0] if len(stock_2023) > 0 and column in stock_2023.columns else 'N/A',
-                    '2024': stock_2024[column].iloc[0] if len(stock_2024) > 0 and column in stock_2024.columns else 'N/A'
+                    'Jan 2025': stock_jan[column].iloc[0] if len(stock_jan) > 0 and column in stock_jan.columns else 'N/A',
+                    'Apr 2025': stock_apr[column].iloc[0] if len(stock_apr) > 0 and column in stock_apr.columns else 'N/A',
+                    'Jun 2025': stock_jun[column].iloc[0] if len(stock_jun) > 0 and column in stock_jun.columns else 'N/A'
                 }
                 comparison_data.append(row)
             
             comparison_df = pd.DataFrame(comparison_data)
 
             actual_stock_name = stock_name  
-            if len(stock_2024) > 0:
-                actual_stock_name = stock_2024['name'].iloc[0]
-            elif len(stock_2023) > 0:
-                actual_stock_name = stock_2023['name'].iloc[0]
-            elif len(stock_2022) > 0:
-                actual_stock_name = stock_2022['name'].iloc[0]
+            if len(stock_jun) > 0:
+                actual_stock_name = stock_jun['name'].iloc[0]
+            elif len(stock_apr) > 0:
+                actual_stock_name = stock_apr['name'].iloc[0]
+            elif len(stock_jan) > 0:
+                actual_stock_name = stock_jan['name'].iloc[0]
 
             st.write('Collecting comparison data for:', actual_stock_name)
             
@@ -57,4 +58,5 @@ if st.button('Get data'):
             st.warning("Please enter a stock name.")
         if not options:
             st.warning("Please select at least one column to compare.")
+
 
