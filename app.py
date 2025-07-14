5388abadf21df5ef0cdb26111a328cae7c38467c
Jiya Ahuja <jia.ahujacod@gmail.com>
2025-07-14 18:38:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -60,7 +60,7 @@ if st.button('Get data'):
             
             comparison_df = pd.DataFrame(comparison_data).astype(str)
             transposed_df = comparison_df.set_index('Metric').T.reset_index()
-            st.table(transposed_df)
+            st.dataframe(transposed_df, use_container_width=False)
 
 
         except Exception as e:
