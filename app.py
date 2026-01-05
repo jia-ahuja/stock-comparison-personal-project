aa37644fd3d0df704b1cb5cb91e6d13b02ac0d66
Jiya Ahuja <jia.ahujacod@gmail.com>
2026-01-05 22:55:40 +0530

    add jan 26 file

--- app.py
+++ app.py
@@ -44,11 +44,13 @@ if st.button('Get data'):
             df_apr = pd.read_excel('file2.xlsx') 
             df_jun = pd.read_excel('file3.xlsx') 
             df_sep = pd.read_excel('file4.xlsx')
+            df_jan26 = pd.read_excel('file5.xlsx')
         
             stock_jan = find_stock(df_jan, stock_name.strip())
             stock_apr = find_stock(df_apr, stock_name.strip())
             stock_jun = find_stock(df_jun, stock_name.strip())
             stock_sep = find_stock(df_sep, stock_name.strip())
+            stock_jan26 = find_stock(df_jan26, stock_name.strip())
             
             comparison_data = []
             
@@ -58,7 +60,8 @@ if st.button('Get data'):
                         'Jan 2025': stock_jan[column].iloc[0] if len(stock_jan) > 0 and column in stock_jan.columns else 'N/A',
                         'Apr 2025': stock_apr[column].iloc[0] if len(stock_apr) > 0 and column in stock_apr.columns else 'N/A',
                         'Jun 2025': stock_jun[column].iloc[0] if len(stock_jun) > 0 and column in stock_jun.columns else 'N/A',
-                        'Sep 2025': stock_sep[column].iloc[0] if len(stock_sep) > 0 and column in stock_sep.columns else 'N/A'
+                        'Sep 2025': stock_sep[column].iloc[0] if len(stock_sep) > 0 and column in stock_sep.columns else 'N/A',
+                        'Jan 2026': stock_jan26[column].iloc[0] if len(stock_jan26) > 0 and column in stock_jan26.columns else 'N/A'
                     }
                     comparison_data.append(row)
                 
@@ -68,7 +71,8 @@ if st.button('Get data'):
                 'Jan 2025': stock_jan['name'].iloc[0] if len(stock_jan) > 0 and 'name' in stock_jan.columns else 'N/A',
                 'Apr 2025': stock_apr['name'].iloc[0] if len(stock_apr) > 0 and 'name' in stock_apr.columns else 'N/A',
                 'Jun 2025': stock_jun['name'].iloc[0] if len(stock_jun) > 0 and 'name' in stock_jun.columns else 'N/A',
-                'Sep 2025': stock_sep['name'].iloc[0] if len(stock_sep) > 0 and 'name' in stock_sep.columns else 'N/A'
+                'Sep 2025': stock_sep['name'].iloc[0] if len(stock_sep) > 0 and 'name' in stock_sep.columns else 'N/A',
+                'Jan 2026': stock_jan26['name'].iloc[0] if len(stock_jan26) > 0 and 'name' in stock_jan26.columns else 'N/A'
             }
             
             comparison_data.insert(0, names_row)
@@ -91,6 +95,7 @@ if st.button('Get data'):
             st.warning("Please enter a stock name.")
         if not options:
             st.warning("Please select at least one column to compare.")
+
 
 
 
